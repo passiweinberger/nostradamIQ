ea4d54e142eaf17e438a199c00b12d7e8104f6df
passiweinberger <passiweinberger@gmail.com>
2015-08-11 21:01:28 +0200

    support for czml

--- nostradamIQ-webapp/proxy/proxy.py
+++ nostradamIQ-webapp/proxy/proxy.py
@@ -7,15 +7,33 @@ import requests
 import base64
 import datetime
 from tinydb import TinyDB, where
+import urllib
+import urllib2
+from urlparse import urlparse
 
 FILTER = False
 CACHE_MIN = 5
 
 
+def makeUrlRequest(url):
+    user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0'
+    url_parsed = urlparse(url)
+    referer = str(urlparsed.scheme) + str(url_parsed.netloc)
+    headers = { 'User-Agent' : user_agent,
+                'Referer': referer,
+                'Origin': origin, 
+                'Access-Control-Allow-Origin': '*' }
+    req = urllib2.Request(url, headers)
+    response = urllib2.(req)
+    response.addHeader('Access-Control-Allow-Origin': '*') 
+    #content = response.read()
+    return response #content
+
+
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
     parameters = parse_qs(environ.get('QUERY_STRING', ''))
-    url = "No url given"
+    url = "No URL given"
     if 'url' in parameters:
         url = parameters['url'][0]
         valid = True
@@ -33,8 +51,8 @@ def app(environ, start_response):
 def getDocByUrl(url):
     cached = False
     db = TinyDB('cache.json')
-    ago = int(((datetime.datetime.now() - datetime.timedelta(minutes=CACHE_MIN)) - datetime.datetime(1970,1,1)).total_seconds())#.timestamp())
-    print(ago)
+    ago = int(((datetime.datetime.now() - datetime.timedelta(minutes=CACHE_MIN)) - datetime.datetime(1970,1,1)).total_seconds()) #.timestamp())
+    #print(ago)
     db.remove(where('date') < ago)
     search = db.search((where('url') == url) & (where('date') > ago))
     if len(search) > 0:
@@ -43,10 +61,11 @@ def getDocByUrl(url):
         #print(search)
         return base64.b64decode(search[0]['content'])
     else:
-        req = requests.get(url)
-        date = int((datetime.datetime.now()-datetime.datetime(1970,1,1)).total_seconds())#.timestamp())
-        db.insert({'url':url,'date':date, 'content':base64.b64encode(req.content)})
-        return req.content
+        req = makeUrlRequest(url) #requests.get(url) #set headers
+        content = req.read()
+        date = int((datetime.datetime.now() - datetime.datetime(1970,1,1)).total_seconds()) #.timestamp())
+        db.insert({'url':url,'date':date, 'content':base64.b64encode(content)})
+        return content
 
 
 WSGIServer(app).run()
