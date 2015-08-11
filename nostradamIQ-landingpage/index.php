0381722fd9b13ba5f3eb17137d70392ea120a434
passiweinberger <passiweinberger@gmail.com>
2015-08-11 13:17:35 +0200

    webapp

--- nostradamIQ-landingpage/index.php
+++ nostradamIQ-landingpage/index.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
+    <meta name="description" content="nostradamIQ - sensing Our World's disasters.">
     <meta name="author" content="nostradamIQ">
     
     <title>nostradamIQ - sensing our World's disasters</title>
